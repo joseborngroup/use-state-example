e2c3faffd4853a0bc9540e5210c58495a588db4b
Jose Paitamala <jose.paitamala@borngroup.com>
2020-07-13 19:07:00 +0100

    React hooks useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import UseStateExample from './components/use-state-example/use-state-example.component';
+import UseEffectExample from './components/use-effect-example/use-effect-example.component';
+
 
 import './App.css';
 
 const App = props => {
-  return <UseStateExample />;
+  //return <UseStateExample />;
+  return <UseEffectExample />;
 };
 
 export default App;
--- src/components/use-effect-example/use-effect-example.component.jsx
+++ src/components/use-effect-example/use-effect-example.component.jsx
@@ -3,39 +3,40 @@ import React, { useState, useEffect } from 'react';
 import Card from '../card/card.component';
 
 const UseEffectExample = () => {
-  const [user, setUser] = useState(null);
-  const [searchQuery, setSearchQuery] = useState('Bret');
+    const [user, setUser] = useState(null);
+    const [searchQuery, setSearchQuery] = useState('Bret');
 
-  useEffect(() => {
-    const fetchFunc = async () => {
-      const response = await fetch(
-        `https://jsonplaceholder.typicode.com/users?username=${searchQuery}`
-      );
-      const resJson = await response.json();
-      setUser(resJson[0]);
-    };
+    useEffect(()=> {
+        const fetchFunc = async () => {
+            const response = await fetch(
+                `https://jsonplaceholder.typicode.com/users?username=${searchQuery}`
+            );
+            const resJson = await response.json();
 
-    fetchFunc();
-  }, [searchQuery]);
+            console.log(resJson);
+            setUser(resJson[0]);
+        };
+        fetchFunc();
+    }, [searchQuery]);
 
-  return (
-    <Card>
-      <input
-        type='search'
-        value={searchQuery}
-        onChange={event => setSearchQuery(event.target.value)}
-      />
-      {user ? (
-        <div>
-          <h3>{user.name}</h3>
-          <h3> {user.username} </h3>
-          <h3> {user.email} </h3>
-        </div>
-      ) : (
-        <p>No user found</p>
-      )}
-    </Card>
-  );
+    return (
+        <Card>
+        <input
+            type='search'
+            value={searchQuery}
+            onChange={event => setSearchQuery(event.target.value)}
+        />
+        {user ? (
+            <div>
+            <h3>{user.name}</h3>
+            <h3> {user.username} </h3>
+            <h3> {user.email} </h3>
+            </div>
+        ) : (
+            <p>No user found</p>
+        )}
+        </Card>
+    );
 };
 
 export default UseEffectExample;
